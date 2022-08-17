ec5a16d9197a7158baba0745de29a6fd0d11561e
Roman <roman_stalmakov@mail.ru>
2022-08-17 18:46:23 +0300

    2. Модель данных. [#174683]

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,13 @@
+package ru.job4j.pojo;
+
+import java.util.Date;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setName("Ilon Musk");
+        student.setGroup("X Æ A-12");
+        student.setStartDate(new Date());
+        System.out.println(student.getName() + "\n" + student.getGroup() + "\n" + student.getStartDate());
+    }
+}
