baa65a6379120b0bfa50285cc317f1e4f7d4a48c
Roman <roman_stalmakov@mail.ru>
2022-08-18 18:43:30 +0300

    9. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,11 +1,8 @@
 package ru.job4j.tracker;
 
-import java.time.format.DateTimeFormatter;
-
 public class StartUI {
     public static void main(String[] args) {
         Item item = new Item();
-        DateTimeFormatter itemFormat = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        System.out.println(item.getCreated().format(itemFormat));
+        System.out.println(item);
     }
 }
