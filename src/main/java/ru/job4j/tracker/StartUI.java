257be2cf7ea63d35353027c85eccc001dcc604f7
Roman <roman_stalmakov@mail.ru>
2022-08-19 16:28:44 +0300

    2.3. Реализация класса StartUI. Добавление заявки [#500744]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -25,7 +25,7 @@ public class StartUI {
     private void showMenu() {
         String[] menu = {
                 "Add new Item", "Show all items", "Edit item",
-                "Delete item", "Find item by id", "Find items by0 name",
+                "Delete item", "Find item by id", "Find items by name",
                 "Exit Program"
         };
         System.out.println("Menu:");
