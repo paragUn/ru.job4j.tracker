157c05236e0875f1017c021daf3fe283e1e32fdd
Roman <roman_stalmakov@mail.ru>
2022-08-19 14:51:46 +0300

    7. Метод удаления заявки Tracker.delete [#211749 #333811]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -60,7 +60,7 @@ public class Tracker {
         boolean rsl = index != -1;
         if (rsl) {
             System.arraycopy(items, index + 1, items, index, size - index - 1);
-            items[size - 1] = null;
+            items[size - 1] =  null;
             size--;
         }
         return rsl;
