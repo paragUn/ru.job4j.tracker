949119f662fdd7eeb1162556b91af7b2a35845b0
Roman <roman_stalmakov@mail.ru>
2022-08-19 12:28:09 +0300

    3. Массивы и модели. [#174892 #333681]

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -5,7 +5,7 @@ public class Library {
         Book[] arrayOfBooks = new Book[4];
         arrayOfBooks[0] = new Book("Faust. Eine Tragödie", 300);
         arrayOfBooks[1] = new Book("Flight of the Eisenstein", 600);
-        arrayOfBooks[2] = new Book("Clean Code", 350);
+        arrayOfBooks[2] = new Book("Clean Code", 351);
         arrayOfBooks[3] = new Book("Necronomicon", 666);
         for (int index = 0; index < arrayOfBooks.length; index++) {
             System.out.println(arrayOfBooks[index].getName() + " " + arrayOfBooks[index].getCountOfPappers());
