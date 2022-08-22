380303e71e8162123451820df2ac3705ed4ae21d
Roman <roman_stalmakov@mail.ru>
2022-08-22 20:10:54 +0300

    9. Написать тесты на StartUI. [#785]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -13,7 +13,7 @@ public class StartUI {
             } else if (select == 2) {
                 StartUI.editItem(input, tracker);
             } else if (select == 3) {
-                StartUI.deteleItem(input, tracker);
+                StartUI.deleteItem(input, tracker);
             } else if (select == 4) {
                 StartUI.findByIdItem(input, tracker);
             } else if (select == 5) {
@@ -56,7 +56,7 @@ public class StartUI {
         }
     }
 
-    public static void deteleItem(Input input, Tracker tracker) {
+    public static void deleteItem(Input input, Tracker tracker) {
         System.out.println("=== Delete item ===");
         int id = Integer.parseInt(input.askStr("Enter id: "));
         if (tracker.delete(id)) {
