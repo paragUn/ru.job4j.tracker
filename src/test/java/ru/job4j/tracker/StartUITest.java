9537580215e369344d8fd83dccc5ccaa6e22a08c
Roman <roman_stalmakov@mail.ru>
2022-08-23 12:05:12 +0300

    5.1. Тестирование. Подготовка данных. [#182960 #335245]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -36,7 +36,7 @@ public class StartUITest {
         Item item = new Item("new item");
         tracker.add(item);
         String[] answers = {
-                String.valueOf(item.getId()), null
+                String.valueOf(item.getId())
         };
         StartUI.deleteItem(new StubInput(answers), tracker);
         assertThat(tracker.findById(item.getId())).isNull();
