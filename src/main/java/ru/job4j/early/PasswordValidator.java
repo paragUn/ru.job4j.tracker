4e3acf43bb73d050973c4ce85f398be5e27e028c
Roman <roman_stalmakov@mail.ru>
2022-08-29 18:39:17 +0300

    4. Принципы раннего возврата и охранных выражений [#504875]

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -1,59 +1,64 @@
 package ru.job4j.early;
 
 public class PasswordValidator {
-    public static String validate(String password) {
-        boolean flagUpperLetter = false;
-        boolean flagLowerLetter = false;
-        boolean flagDigit = false;
-        boolean flagSpecialSym = false;
+    private static boolean flagContainUpperLetter = false;
+    private static boolean flagContainLowerLetter = false;
+    private static boolean flagContainDigit = false;
+    private static boolean flagContainSpecialSym = false;
+    private static boolean flagContainSubstring = false;
 
-        if (password == null) {
+    public static String validate(String password) {
+        if (password == null || password.isEmpty()) {
             throw new IllegalArgumentException("Empty password.");
         }
-
         if (password.length() < 8 || password.length() > 32) {
-            return "The password length must be in the range [8,21]!!";
+            return "The password length must be in the range [8,32]!!";
         }
+        validateSymbols(password);
+        if (!flagContainDigit) {
+            return "The password must contain numbers";
+        }
+        if (!flagContainUpperLetter) {
+            return "The password must contain upper letter";
+        }
+        if (!flagContainLowerLetter) {
+            return "The password must contain lower letter";
+        }
+        if (!flagContainSpecialSym) {
+            return "The password must contain special symbols";
+        }
+        if (!containSubstring(password)) {
+            return "The password must not contain substrings: qwerty, 12345, password, admin, user!!";
+        }
+        return password;
+    }
 
+    private static void validateSymbols(String password) {
         for (int index = 0; index < password.length(); index++) {
             char current = password.charAt(index);
             if (Character.isDigit(current)) {
-                flagDigit = true;
-            }
-            if (Character.isLowerCase(current)) {
-                flagLowerLetter = true;
+                flagContainDigit = true;
             }
             if (Character.isUpperCase(current)) {
-                flagUpperLetter = true;
+                flagContainUpperLetter = true;
+            }
+            if (Character.isLowerCase(current)) {
+                flagContainLowerLetter = true;
             }
             if ((current > 31 && current < 48)
                     || (current > 57 && current < 64)
                     || (current > 90 && current < 97)
                     || (current > 122 && current < 127)) {
-                flagSpecialSym = true;
+                flagContainSpecialSym = true;
             }
         }
-
-        if (!flagDigit) {
-            return "The password must contain numbers";
-        }
-
-        if (!flagUpperLetter) {
-            return "The password must contain upper Letter";
-        }
-
-        if (!flagLowerLetter) {
-            return "The password must contain lower letter";
-        }
-
-        if (!flagSpecialSym) {
-            return "The password must contain lower letter";
-        }
-
-    return password;
     }
 
-    public static void main(String[] args) {
-        System.out.println(Character.isAlphabetic(123));
+    private static boolean containSubstring(String password) {
+            return password.contains("qwerty")
+                || password.contains("12345")
+                || password.contains("password")
+                || password.contains("admin")
+                || password.contains("user");
     }
 }
---
+++ src/test/java/ru/job4j/early/PasswordValidatorTest.java
@@ -0,0 +1,121 @@
+package ru.job4j.early;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PasswordValidatorTest {
+
+    @Test
+    public void whenValidPassword() {
+        String in = "MG+7n8Fz}^C74p";
+        String expected = "MG+7n8Fz}^C74p";
+        String result = PasswordValidator.validate(in);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenNullPassword() {
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> PasswordValidator.validate(null));
+        assertThat(exception.getMessage()).isEqualTo("Empty password.");
+    }
+
+    @Test
+    public void whenEmptyPassword() {
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> PasswordValidator.validate(""));
+        assertThat(exception.getMessage()).isEqualTo("Empty password.");
+    }
+
+    @Test
+    public void whenLengthLessThan8Password() {
+        String in = "MG+7n";
+        String expected = "The password length must be in the range [8,32]!!";
+        String result = PasswordValidator.validate(in);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenLengthGreaterThan32Password() {
+        String in = "ni|P,Qsa0F874{.RJ|v50K5{aLO%3&bt0";
+        String expected = "The password length must be in the range [8,32]!!";
+        String result = PasswordValidator.validate(in);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenDoesNotContainNumber() {
+        String in = "MG+nFz}^Cp";
+        String expected = "The password must contain numbers";
+        String result = PasswordValidator.validate(in);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenDoesNotContainUpperLetter() {
+        String in = "+7n8z}^74p";
+        String expected = "The password must contain upper letter";
+        String result = PasswordValidator.validate(in);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenDoesNotContainLowerLetter() {
+        String expected = "The password must contain lower letter";
+        String in = "MG+78F}^C74";
+        String result = PasswordValidator.validate(in);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenDoesNotContainSpecialSymbols() {
+        String expected = "The password must contain special symbols";
+        String in = "MG7n8FzC74p";
+        String result = PasswordValidator.validate(in);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenContainQwerty() {
+        String expected = "The password must not contain substrings: qwerty, 12345, password, admin, user!!";
+        String in = "MG+7n8Fz}^C74pQwErTy";
+        String result = PasswordValidator.validate(in);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenContain12345() {
+        String expected = "The password must not contain substrings: qwerty, 12345, password, admin, user!!";
+        String in = "MG+7n8F12345z}^C74p";
+        String result = PasswordValidator.validate(in);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenContainLiterallyPassword() {
+        String expected = "The password must not contain substrings: qwerty, 12345, password, admin, user!!";
+        String in = "MG+password7n8Fz}^C74p";
+        String result = PasswordValidator.validate(in);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenContainLiterallyAdmin() {
+        String expected = "The password must not contain substrings: qwerty, 12345, password, admin, user!!";
+        String in = "MG+7n8Fz}^CAdMiN74p";
+        String result = PasswordValidator.validate(in);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenContainLiterallyUsEr() {
+        String expected = "The password must not contain substrings: qwerty, 12345, password, admin, user!!";
+        String in = "MG+7n8Fz}UsEr^CAdMiN74p";
+        String result = PasswordValidator.validate(in);
+        assertThat(result).isEqualTo(expected);
+    }
+}
