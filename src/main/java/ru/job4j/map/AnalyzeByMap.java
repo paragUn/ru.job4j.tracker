f656cd941c1f1fd4bf4c9333b7915eec2570a60b
Roman <roman_stalmakov@mail.ru>
2022-09-14 11:31:41 +0300

    4. Аттестация   [#504878 #342461]

--- src/main/java/ru/job4j/map/AnalyzeByMap.java
+++ src/main/java/ru/job4j/map/AnalyzeByMap.java
@@ -36,7 +36,7 @@ public class AnalyzeByMap {
         Map<String, Integer> temp = new LinkedHashMap<>();
         for (Pupil pupil : pupils) {
             for (Subject subject : pupil.subjects()) {
-                temp.merge(subject.name(), subject.score(), (a, b) -> b + temp.get(subject.name()));
+                temp.merge(subject.name(), subject.score(), (a, b) -> a + subject.score());
             }
         }
         for (Map.Entry<String, Integer> map : temp.entrySet()) {
@@ -64,11 +64,7 @@ public class AnalyzeByMap {
         List<Label> result = new ArrayList<>();
         for (Pupil pupil : pupils) {
             for (Subject subject : pupil.subjects()) {
-                if (temp.containsKey(subject.name())) {
-                    temp.put(subject.name(), subject.score() + temp.get(subject.name()));
-                } else {
-                    temp.put(subject.name(), subject.score());
-                }
+                temp.merge(subject.name(), subject.score(), (a, b) -> a + subject.score());
             }
         }
         for (Map.Entry<String, Integer> map : temp.entrySet()) {
