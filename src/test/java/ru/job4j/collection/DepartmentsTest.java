92b3573bec881e74d3a3ee8a68f3917dabdf2dc3
roman <roman_stalmakov@mail.ru>
2022-09-19 00:56:38 +0300

    2. Отсортировать департаменты [#34352]

--- src/test/java/ru/job4j/collection/DepartmentsTest.java
+++ src/test/java/ru/job4j/collection/DepartmentsTest.java
@@ -22,7 +22,6 @@ public class DepartmentsTest {
         List<String> input = Arrays.asList("k1", "k1/sk1");
         List<String> expect = Arrays.asList("k1", "k1/sk1");
         List<String> result = Departments.fillGaps(input);
-        System.out.println(result);
         assertThat(result).containsAll(expect);
     }
 
