1a90d5c97116197902fca5dc3873f30d37ce3fc8
Roman <roman_stalmakov@mail.ru>
2022-09-30 13:11:47 +0300

    1. Метод .of [#70623]

--- src/main/java/ru/job4j/collection/Departments.java
+++ src/main/java/ru/job4j/collection/Departments.java
@@ -9,7 +9,7 @@ public class Departments {
         for (String value : deps) {
             String start = "";
             for (String el : value.split("/")) {
-                start = "".equals(start) ? start + el : start + "/" + el;
+                start += "".equals(start) ? el : "/" + el;
                 tmp.add(start);
             }
         }
